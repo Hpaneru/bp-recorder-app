3d206b49e391e1f72b3588cdc2ad3a728649aa24
Hemant <hpaneru.hp@gmail.com>
2022-03-17 20:32:30 +0545

    🎉🎉project initialized🎉🎉

---
+++ lib/screens/add_record_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddRecordScreen extends StatelessWidget {
+  const AddRecordScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
